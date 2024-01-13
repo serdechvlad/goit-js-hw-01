3676afa921c2b59a3e6594fcb3cdc1c04b87219e
serdechvlad <serdechvlad94@gmail.com>
2024-01-13 21:28:50 +0100

    ф

--- js/task-3.js
+++ js/task-3.js
@@ -9,4 +9,5 @@ return width;
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
 console.log(getElementWidth('200px', '0px', '0px')); // 200
- 
+
+console.log("0" == false);
